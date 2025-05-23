9724330f3a5b249f7cdc7d22b2bf4f68a4af9927
DESKTOP-HFQGOT6\Radek <tukedarr@gmail.com>
2025-05-23 13:04:56 +0200

    Meta tags, sitemap, roboto, fix mobile navigation menu + menu links hover, section slanted mobile view correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     output: "server", // ważne, jeśli chcesz używać funkcji Netlify
     adapter: netlify(),
+    site: 'https://capybit.pl',
     experimental: {
         session: true,
     },
@@ -13,5 +15,10 @@ export default defineConfig({
         server: {
             allowedHosts: ['.ngrok-free.app'], // wildcard dopuszcza każdy tunel ngrok
         }
-    }
+    },
+    integrations: [
+        sitemap({
+
+        }),
+  ],
 });
