dce8e0e69b28488b86971b0e6c3513455362b95a
DESKTOP-HFQGOT6\Radek <tukedarr@gmail.com>
2025-12-06 18:27:34 +0100

    Remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server", // ważne, jeśli chcesz używać funkcji Netlify
-    adapter: netlify(),
-    site: 'https://capybit.pl',
-    experimental: {
-        session: true,
-    },
-    vite: {
-        server: {
-            allowedHosts: ['.ngrok-free.app'], // wildcard dopuszcza każdy tunel ngrok
-        }
-    },
-    integrations: [
-        sitemap({
-
-        }),
-  ],
+  site: 'https://HexagonTournamentPage.github.io',
+  base: '/HexagonTournamentPage',
 });
