6d540465777d1d40e7665e568d5677bdef1491ce
DESKTOP-HFQGOT6\Radek <tukedarr@gmail.com>
2025-12-06 18:35:20 +0100

    Update config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://HexagonTournamentPage.github.io',
+  site: 'https://capybitcompany.github.io',
   base: '/HexagonTournamentPage',
 });
