458eead307e3975ac21a1b7843944110af575246
DESKTOP-HFQGOT6\Radek <tukedarr@gmail.com>
2026-04-15 22:05:13 +0200

    Visual changes of theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://capybitcompany.github.io',
+  site: 'https://www.minecrafthex.pl',
   base: '/',
   integrations: [sitemap()],
 });
